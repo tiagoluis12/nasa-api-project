6c880ddc955fe13b40bba5432d2ef2feb354310c
“tiagoluis12” <tiago7164@hotmail.com>
2025-02-24 21:44:35 +0000

    Services: Api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://nasa-api-project-backend.vercel.app/api";
 
 export const fetchSearchResults = async (query, mediaType = "all") => {
   try {
